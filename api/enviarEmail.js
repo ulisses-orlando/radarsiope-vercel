51654019c6c0493ab8e978375705a1398641d75d
Ulisses-Orlando <ulisses.orlando@gmail.com>
2025-10-14 20:55:11 -0300

    corrigido CORS para permitir requisições do formulário

--- api/enviarEmail.js
+++ api/enviarEmail.js
@@ -1,3 +1,14 @@
+export default async function handler(req, res) {
+  // ✅ Cabeçalhos CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // ✅ Responder pré-requisição OPTIONS
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
 const nodemailer = require("nodemailer");
 
 module.exports = async (req, res) => {
