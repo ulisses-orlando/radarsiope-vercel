46fe1628227b407cf7015d7e7813267daadde0cf
ulisses-orlando <ulisses.orlando@gmail.com>
2025-12-10 19:55:52 -0300

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -506,7 +506,7 @@ async function abrirModalNewsletter(docId = null, isEdit = false) {
 
     // Monta o código de rastreamento
     const texto = `
-    <a href="https://api.radarsiope.com.br/api/click?newsletter={{newsletterId}}&email={{email}}&url=${encodeURIComponent(destino)}">
+    <a href="https://api.radarsiope.com.br/api/click?newsletterId={{newsletterId}}&email={{email}}&url=${encodeURIComponent(destino)}">
       Clique aqui para acessar o conteúdo
     </a>
   `;
