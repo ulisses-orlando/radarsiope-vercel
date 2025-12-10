c951ff5f0cfe12ef7df556be7acfcbd5b5d9f33b
ulisses-orlando <ulisses.orlando@gmail.com>
2025-12-10 19:23:00 -0300

    Fix escape sequences in unsubscribe.js

--- api/unsubscribe.js
+++ api/unsubscribe.js
@@ -6,7 +6,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\\\n/g, '\n')
     })
   });
 }
@@ -58,3 +58,4 @@ export default async function handler(req, res) {
     return res.status(500).send("❌ Erro interno no servidor.");
   }
 }
+
