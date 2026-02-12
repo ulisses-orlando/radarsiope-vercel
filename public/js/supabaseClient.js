79f7a3444fa4426ab4cfd88b8e920e15b79bf5ea
ulisses-orlando <ulisses.orlando@gmail.com>
2026-02-12 15:33:45 -0300

    Add files via upload

--- public/js/supabaseClient.js
+++ public/js/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
 // As variáveis vêm do .env (configuradas no Vercel)
-const supabaseUrl = process.env.VITE_SUPABASE_URL
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = window.env.VITE_SUPABASE_URL
+const supabaseAnonKey = window.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
