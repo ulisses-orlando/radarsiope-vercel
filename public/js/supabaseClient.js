de57f0f34ae55909bc1ab27b9e962a3f0c64e01f
ulisses-orlando <ulisses.orlando@gmail.com>
2026-02-12 16:29:02 -0300

    Add files via upload

--- public/js/supabaseClient.js
+++ public/js/supabaseClient.js
@@ -3,6 +3,9 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = window.env.VITE_SUPABASE_URL
 const supabaseAnonKey = window.env.VITE_SUPABASE_ANON_KEY
 
+console.log("Supabase URL:", supabaseUrl) 
+console.log("Supabase Anon Key:", supabaseAnonKey ? "OK (não mostrar chave completa)" : "NÃO DEFINIDA")
+
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 // expõe globalmente
