42cbd1cb76e28cc6cd123071c6436440b7298361
ulisses-orlando <ulisses.orlando@gmail.com>
2026-02-13 14:19:02 -0300

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Vite injeta as variáveis de ambiente do Vercel
 const supabaseUrl = 'https://ekrtekidjuwxfspjmmvl.supabase.co'
-const supabaseAnonKey = 'sb_publishable_O4cgL1aB5kqTyd0turl_cw_t9JkvK7u'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrcnRla2lkanV3eGZzcGptbXZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0OTQzNTAsImV4cCI6MjA4NjA3MDM1MH0.tm2y114xrBHeRU62beNln8TQmskm2yxap7MiRIAHhoc'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
