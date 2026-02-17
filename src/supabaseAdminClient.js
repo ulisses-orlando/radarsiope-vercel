ee470e7932c673fce390d59f462eb1e5886964a8
ulisses-orlando <ulisses.orlando@gmail.com>
2026-02-17 18:18:40 -0300

    Add files via upload

--- src/supabaseAdminClient.js
+++ src/supabaseAdminClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ekrtekidjuwxfspjmmvl.supabase.co';
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrcnRla2lkanV3eGZzcGptbXZsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDQ5NDM1MCwiZXhwIjoyMDg2MDcwMzUwfQ.3Kjj1_lgrOe2XeiaCHTrrkBfind8PkehmxNnKxz0bms';
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
